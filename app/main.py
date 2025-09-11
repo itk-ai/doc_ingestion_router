28c267ed51638a64f9d3514278f6b98c7195eb83
Jesper Kristensen <cableman@linuxdev.dk>
2025-09-11 13:11:39 +0200

    4933: Added metrics and token handling

--- app/main.py
+++ app/main.py
@@ -1,16 +1,26 @@
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI, HTTPException, Depends
 from app.config import settings
 from app.services.tika import TikaService
 from app.api.models import HealthResponse
 from app.api.endpoints import router as api_router
+from app.core.security import get_bearer_token
 from loguru import logger
+from prometheus_fastapi_instrumentator import Instrumentator
 
 
 app = FastAPI(
     title=settings.APP_NAME,
     description="A service to route document loading requests to appropriate Tika endpoints",
-    version="1.0.0"
+    version="1.0.0",
 )
+
+# Initialize Prometheus monitoring
+Instrumentator().instrument(app).expose(
+    app,
+    include_in_schema=False,
+    dependencies=[Depends(get_bearer_token, use_cache=False)],
+)
+
 
 # Public endpoints (no auth required)
 @app.get("/health", tags=["Health"], response_model=HealthResponse)
@@ -21,11 +31,11 @@ async def health_check():
     if not tika_available:
         raise HTTPException(status_code=503, detail="Tika service is not available")
 
-    logger.info(f"A health check was requested. Tika service, and service {settings.APP_NAME} is up and running.")
-    return {
-        "status": "healthy",
-        "service": settings.APP_NAME
-    }
+    logger.info(
+        f"A health check was requested. Tika service, and service {settings.APP_NAME} is up and running."
+    )
+    return {"status": "healthy", "service": settings.APP_NAME}
+
 
 # Include the API router
 app.include_router(api_router, prefix="/api/v1")
